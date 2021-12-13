3d8ba71458988279ed5353933984bbf2a7bcda2d
JUNGHOJONG <hojong1351@naver.com>
2021-12-13 23:18:48 +0900

    mustache 연습

--- src/main/java/com/davinci/web/UserController.java
+++ src/main/java/com/davinci/web/UserController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 회원가입, 회원목록 조회
@@ -18,6 +17,42 @@ public class UserController {
 
     // static or non-static
     private final List<User> users = new ArrayList<>();
+
+    /**
+     * mustache 연습용
+     */
+    @GetMapping("/mustacheTest")
+    public String mustacheTest(Model model) {
+        model.addAttribute("name", "Chris");
+        model.addAttribute("value", 10000);
+        model.addAttribute("taxed_value", 10000 - (10000 * 0.4));
+        model.addAttribute("in_ca", true);
+
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
+        model.addAttribute("nothin", list);
+
+        HashMap<String, String> hashMap1 = new HashMap<>();
+        hashMap1.put("name", "one");
+        HashMap<String, String> hashMap2 = new HashMap<>();
+        hashMap2.put("name", "two");
+        HashMap<String, String> hashMap3 = new HashMap<>();
+        hashMap3.put("name", "three");
+
+        List<Map<String, String>> mapList = Arrays.asList(hashMap1, hashMap2, hashMap3);
+        model.addAttribute("repo", mapList);
+
+        HashMap<String, String> hashMap4 = new HashMap<>();
+        hashMap4.put("name", "Willy");
+        HashMap<String, String> hashMap5 = new HashMap<>();
+        hashMap5.put("wrapped", "((text: String) => <b>{text}</b>)");
+
+        List<Map<String, String>> mapList1 = Arrays.asList(hashMap4, hashMap5);
+
+        model.addAttribute("wrapped", mapList1);
+
+        return "test";
+    }
 
     /**
      * test 용
